5081747603e19e73718a5ed0868ea8912f1a6feb
wowus94 <sesila94@mail.ru>
2022-01-26 20:11:28 +0300

    Calculator v1.0

---
+++ app/src/main/java/com/example/calculator_gb/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculator_gb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity);
+    }
+}
